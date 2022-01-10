63513a5524aaaa5a40e3bc38a7fc303ba4bd58ba
Brymir7 <brymir7@gmail.com>
2022-01-10 18:31:41 +0100

    Caesar

---
+++ decryption1.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char **argv)
+{   
+    char input[100], single_char;
+    int key;
+    
+    printf("Message to decrypt: ");
+    gets(input);
+    printf("Enter key: ");
+    scanf("%d", &key);
+    for(int i = 0; input[i] != 0; ++i){
+        single_char = input[i];
+        single_char -= key;
+        input[i] = single_char;
+    }
+    printf("Encrypted Message: %s", input);
+    return 0;
+}
